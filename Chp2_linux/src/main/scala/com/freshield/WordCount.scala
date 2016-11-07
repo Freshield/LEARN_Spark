737ef39d4af328c191e214b527adde493e634010
Freshield <zxdsw199182@gmail.com>
2016-11-06 20:07:51 -0500

    done chp2

--- Chp2_linux/src/main/scala/com/freshield/WordCount.scala
+++ Chp2_linux/src/main/scala/com/freshield/WordCount.scala
@@ -1,7 +1,7 @@
 package com.freshield
 
 /**
-  * Created by FRESHIELD on 2016/11/6.
+  * Created by freshield on 16-11-6.
   */
 
 import org.apache.spark.SparkConf
@@ -10,13 +10,14 @@ import org.apache.spark.SparkContext._
 
 object WordCount {
 
-  def main(args: Array[String]) {
-    val conf = new SparkConf().setMaster("local").setAppName("wordCount")
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("wordCount")
     val sc = new SparkContext(conf)
     val input = sc.textFile("file:///mnt/usb/spark-1.2.0-bin-hadoop2.4/README.md")
     val words = input.flatMap(line => line.split(" "))
     val counts = words.map(word => (word, 1)).reduceByKey{case (x, y) => x + y}
-    counts.saveAsTextFile("file:///mnt/usb/WordCount.txt")
+    counts.foreach(println)
+    counts.saveAsTextFile("file:///mnt/usb/WordCount")
   }
 
 }
