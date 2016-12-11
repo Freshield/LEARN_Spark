c8926e10433eed132681a67cd7c19a4cd2a95e35
freshield <zxdsw199182@gmail.com>
2016-12-10 21:45:11 -0500

    done

--- Chp10/src/main/scala/com/freshield/StreamingTest.scala
+++ Chp10/src/main/scala/com/freshield/StreamingTest.scala
@@ -3,6 +3,21 @@ package com.freshield
 /**
   * Created by freshield on 11/12/16.
   */
-object StreamingTest {
+import org.apache.spark._
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.StreamingContext._
 
+object StreamingTest {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
+    val ssc = new StreamingContext(conf, Seconds(1))
+    val lines = ssc.socketTextStream("localhost",7777)
+    val words = lines.flatMap(_.split(" "))
+    val pairs = words.map(word => (word,1))
+    val wordCount = pairs.reduceByKey(_+_)
+    wordCount.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
 }
