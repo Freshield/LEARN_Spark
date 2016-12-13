f411261eceb410e6ea24a425b1c0ef800f4acd6d
freshield <zxdsw199182@gmail.com>
2016-12-12 21:24:54 -0500

    streaming begin

--- Chp10_5/src/main/scala/com/freshield/StreamingTest.scala
+++ Chp10_5/src/main/scala/com/freshield/StreamingTest.scala
@@ -3,10 +3,23 @@ package com.freshield
 /**
   * Created by freshield on 15/12/16.
   */
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.StreamingContext._
+import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.streaming.Duration
+import org.apache.spark.streaming.Seconds
+import org.apache.spark.SparkConf
+
 object StreamingTest {
 
   def main(args: Array[String]): Unit = {
-
+    val conf = new SparkConf().setAppName("Streaming Test")
+    val ssc = new StreamingContext(conf,Seconds(1))
+    val lines = ssc.socketTextStream("localhost",7777)
+    val errorLInes = lines.filter(_.contains("error"))
+    errorLInes.print()
+    ssc.start()
+    ssc.awaitTerminatio
   }
 
 }
